252ecdd41c796caf46cd88ab442cb2d4d52b4f54
Fábio Silva <fabio.fernando.osilva@gmail.com>
2019-06-27 19:00:19 -0300

    Allow filter function to be imported
    Also rename: filter() -> CrossWordsFilter()

--- word_search.py
+++ word_search.py
@@ -1,6 +1,6 @@
 word_file_path = 'words.txt'
 
-def filter(word, letters=None, length=None, positions=None):
+def CrossWordsFilter(word, letters=None, length=None, positions=None):
     found = True
 
     if length is not None and len(word) != length:
@@ -21,22 +21,23 @@ def filter(word, letters=None, length=None, positions=None):
 
     return found
 
-i_letters = input("Letters: ")
+if __name__ == "__main__":
+    i_letters = input("Letters: ")
 
-while True:
-    i_num_letters = int(input("Num. of letters (0 to quit): "))
+    while True:
+        i_num_letters = int(input("Num. of letters (0 to quit): "))
 
-    if i_num_letters == 0: break
+        if i_num_letters == 0: break
 
-    dict_pos = {}
-    for i in range(1, i_num_letters + 1):
-        letter_at_pos = None
-        letter_at_pos = input("Letter at position {:d}: ".format(i))
-        if letter_at_pos is not '':
-            dict_pos[i] = letter_at_pos
+        dict_pos = {}
+        for i in range(1, i_num_letters + 1):
+            letter_at_pos = None
+            letter_at_pos = input("Letter at position {:d}: ".format(i))
+            if letter_at_pos is not '':
+                dict_pos[i] = letter_at_pos
 
-    with open(word_file_path, 'r') as word_file:
-        for line in word_file:
-            word = line.strip().lower()
-            if filter(word, i_letters, i_num_letters, dict_pos):
-                print(word)
+        with open(word_file_path, 'r') as word_file:
+            for line in word_file:
+                word = line.strip().lower()
+                if filter(word, i_letters, i_num_letters, dict_pos):
+                    print(word)
